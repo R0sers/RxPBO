ee15627cd11707f4895493f40058fcc34354f1b0
Robi Septiandi <robisave21@gmail.com>
2025-11-01 19:39:41 +0700

    RobiS_5520124028_PBOPrakP5

---
+++ RobiS_5520124028_PBOPrakP5/No4.java
@@ -0,0 +1,22 @@
+package Robis_5520124028_PBOPrakP5;
+
+import java.util.Scanner;
+
+public class No4 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Masukkan banyak angka: ");
+        int n = input.nextInt();
+
+        int total = 0;
+        int angka = 1;
+
+        for (int i = 1; i <= n; i++) {
+            System.out.print(angka + " ");
+            total += angka;
+            angka += 2;
+        }
+
+        System.out.println("\nTotal penjumlahan: " + total);
+    }
+}
