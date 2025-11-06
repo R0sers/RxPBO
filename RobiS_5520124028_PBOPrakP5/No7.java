841d236bb01cb3ea2f9194b4981ed94e5b71b57c
Robi Septiandi <robisave21@gmail.com>
2025-11-06 17:01:20 +0700

    Tugas PBO Robi S 5520124028 P5

---
+++ RobiS_5520124028_PBOPrakP5/No7.java
@@ -0,0 +1,25 @@
+
+package RobiS_5520124028_PBOPrakP5;
+
+public class No7 {
+    public static void main(String[] args){
+        
+        int i=0;
+        System.out.println("i++");
+        while (i<5){
+            System.out.println("i++ : " + i);
+            i++;
+        }
+        
+        System.out.println("");
+        i = 0;
+        System.out.println("++i");
+        while (i<5){
+            ++i;
+            System.out.println("i++ : " + i);
+        }
+            
+        
+        
+    }
+}
